e70c781339fcacd790680f955308999e5bad330d
michaelb <michaelpb@gmail.com>
2018-12-03 16:06:19 -0800

    tweak

--- client/src/components/pages/LandingPage/LandingPage.js
+++ client/src/components/pages/LandingPage/LandingPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link, Switch, Route } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import './LandingPage.css';
 class LandingPage extends Component {
